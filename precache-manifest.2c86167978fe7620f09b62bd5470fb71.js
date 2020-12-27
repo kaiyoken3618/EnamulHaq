04dd2f9c610c668925782e2b754ce46b504968ef
Enamul Haq Chowdhury <43555641+kaiyoken3618@users.noreply.github.com>
2020-12-27 22:38:10 +0600

    Updates

---
+++ precache-manifest.2c86167978fe7620f09b62bd5470fb71.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49320bb3a84bfec7618c8a2ff57d32e",
+    "url": "/EnamulHaq/index.html"
+  },
+  {
+    "revision": "d07e0fe9131203c80b2b",
+    "url": "/EnamulHaq/static/css/main.4e7073b5.chunk.css"
+  },
+  {
+    "revision": "bf328ecd9779a0d0e2f7",
+    "url": "/EnamulHaq/static/js/2.fed9c131.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/EnamulHaq/static/js/2.fed9c131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07e0fe9131203c80b2b",
+    "url": "/EnamulHaq/static/js/main.09e877be.chunk.js"
+  },
+  {
+    "revision": "753e7a340d8ec429ed00",
+    "url": "/EnamulHaq/static/js/runtime-main.027d1f84.js"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/EnamulHaq/static/media/C.f278be79.png"
+  },
+  {
+    "revision": "29cbbbbb9989b603bcf27f4c9a62845e",
+    "url": "/EnamulHaq/static/media/accenture-3.29cbbbbb.jpg"
+  },
+  {
+    "revision": "1a632d2b39f3275752f2c4b3c73477a3",
+    "url": "/EnamulHaq/static/media/angular.1a632d2b.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/EnamulHaq/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/EnamulHaq/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "c0bfd9544966c5480aede52db7257e04",
+    "url": "/EnamulHaq/static/media/bankdb.c0bfd954.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/EnamulHaq/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "84ddfbe707a812896b723147a1cd718e",
+    "url": "/EnamulHaq/static/media/colab.84ddfbe7.png"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/EnamulHaq/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/EnamulHaq/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/EnamulHaq/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "a0535bb66a1a5491a67d767e38ba0b6e",
+    "url": "/EnamulHaq/static/media/ecom.a0535bb6.jpg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/EnamulHaq/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/EnamulHaq/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/EnamulHaq/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/EnamulHaq/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "07c1c28bf34c5994ad0b51aa916f7d52",
+    "url": "/EnamulHaq/static/media/image_classification.07c1c28b.PNG"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/EnamulHaq/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "d75acc6b246aa33e1e310c97d54637e4",
+    "url": "/EnamulHaq/static/media/json.d75acc6b.png"
+  },
+  {
+    "revision": "aba3531c54d2a85939da13dda7b93350",
+    "url": "/EnamulHaq/static/media/keras.aba3531c.png"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/EnamulHaq/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/EnamulHaq/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/EnamulHaq/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "cdf5b5e1a8dd51c1130b4d4a86048b8a",
+    "url": "/EnamulHaq/static/media/php.cdf5b5e1.svg"
+  },
+  {
+    "revision": "0e0e0756ff469a695d3b078662340deb",
+    "url": "/EnamulHaq/static/media/plsql.0e0e0756.png"
+  },
+  {
+    "revision": "fd124f93b2cb40e7e73c6fc44438118e",
+    "url": "/EnamulHaq/static/media/portfolio.fd124f93.jpg"
+  },
+  {
+    "revision": "95dd3e00f0920b6bed086d4a30d5b027",
+    "url": "/EnamulHaq/static/media/portfolioUsingDjango.95dd3e00.webp"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/EnamulHaq/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "f605771c9281572769b5b8302382a30f",
+    "url": "/EnamulHaq/static/media/profile.f605771c.PNG"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/EnamulHaq/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/EnamulHaq/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/EnamulHaq/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/EnamulHaq/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/EnamulHaq/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "bf5a6f412e4b095fb5b22dd09f4afbe4",
+    "url": "/EnamulHaq/static/media/resto.bf5a6f41.PNG"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/EnamulHaq/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "66d4de73a2d4fb06bd852fc5720b7036",
+    "url": "/EnamulHaq/static/media/slide1.66d4de73.jpg"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/EnamulHaq/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/EnamulHaq/static/media/slide3.8bce7aef.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/EnamulHaq/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/EnamulHaq/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "dd9fc2f19c878cdffa40d55bb219a86e",
+    "url": "/EnamulHaq/static/media/vs.dd9fc2f1.png"
+  },
+  {
+    "revision": "1a7895cd654a1526c30c80b188babc4e",
+    "url": "/EnamulHaq/static/media/wix.1a7895cd.png"
+  },
+  {
+    "revision": "2e2cbb5e6bc6f1531149f12f0d43c31d",
+    "url": "/EnamulHaq/static/media/wordpress.2e2cbb5e.png"
+  },
+  {
+    "revision": "d0028189da7645b6496cd8c94c37b183",
+    "url": "/EnamulHaq/static/media/wpblog.d0028189.PNG"
+  },
+  {
+    "revision": "9aff2eb9c6cad735d22fc607b397de6b",
+    "url": "/EnamulHaq/static/media/xamp.9aff2eb9.jpg"
+  }
+]);
